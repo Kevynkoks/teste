dddedee8832aef2d7abc22a4fbcbecd8dd3028fe
Kevynkoks <kevyn.santana@escola.pr.gov.br>
2025-03-20 12:21:20 -0300

    Add files via upload

---
+++ teste 1.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int numero, gorjeta, conta;
+     scanf("%d",&numero);
+     gorjeta=numero/10;
+ 
+    
+    printf("conta:%d",numero + gorjeta);
+
+    return 0;
+}
